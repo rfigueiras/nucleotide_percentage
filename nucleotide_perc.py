93f1ad8317d04d710e1d29b7c11e8d45ffe33262
antoniojperez <antoniojperez@hotmail.com>
2024-06-02 14:56:33 -0400

    I essentially took some actions to classify the sequence into DNA, RNA or wrong. After that the code applies a slightly different function to calculate the percentage of nucleotides depending on the nature of the sequence or indicates the sequence is wrong.

--- nucleotide_perc.py
+++ nucleotide_perc.py
@@ -13,10 +13,11 @@ if len(sys.argv) == 1:
 
 # Parse the arguments
 args = parser.parse_args()
+args.seq = args.seq.upper()     # I found it more logical to change the sequence to upper case at this point, but let me know if you like it
 
-def calculate_nucleotide_percentage(sequence):
+def calculate_nucleotide_percentage_DNA(sequence):
     # Handling case sensitivity
-    sequence = sequence.upper()
+    sequence = sequence.upper()     # this line is redundant now, we could eliminate it I think
     
     # total length of the sequence
     total_length = len(sequence)
@@ -27,7 +28,6 @@ def calculate_nucleotide_percentage(sequence):
     count_c = sequence.count('C')
     count_g = sequence.count('G')
     
-    # Calculate the %  of each nucleotide
     percentage_a = (count_a / total_length) * 100
     percentage_t = (count_t / total_length) * 100
     percentage_c = (count_c / total_length) * 100
@@ -36,10 +36,41 @@ def calculate_nucleotide_percentage(sequence):
     return percentage_a, percentage_t, percentage_c, percentage_g
 
 
-percentage_a, percentage_t, percentage_c, percentage_g = calculate_nucleotide_percentage(args.seq)
+def calculate_nucleotide_percentage_RNA (sequence):
+    # Handling case sensitivity
+    sequence = sequence.upper()     # this line is redundant now, we could eliminate it
+
+    # total length of the sequence
+    total_length = len(sequence)
+
+    # Count number of nucleotide on sequence
+    count_a = sequence.count('A')
+    count_u = sequence.count('U')
+    count_c = sequence.count('C')
+    count_g = sequence.count('G')
+
+    # Calculate the %  of each nucleotide
+    percentage_a = (count_a / total_length) * 100
+    percentage_u = (count_u / total_length) * 100
+    percentage_c = (count_c / total_length) * 100
+    percentage_g = (count_g / total_length) * 100
+
+    return percentage_a, percentage_u, percentage_c, percentage_g
 
 
-print(f"Percentage of A:", percentage_a)
-print(f"Percentage of T:", percentage_t)
-print(f"Percentage of C:", percentage_c)
-print(f"Percentage of G:",  percentage_g)
+if re.search('^[ACGT]+$', args.seq):    # selecting the right function for DNA
+    percentage_a, percentage_t, percentage_c, percentage_g = calculate_nucleotide_percentage_DNA(args.seq)
+    print(f"Percentage of A:", percentage_a)
+    print(f"Percentage of T:", percentage_t)
+    print(f"Percentage of C:", percentage_c)
+    print(f"Percentage of G:",  percentage_g)
+elif re.search('^[ACGU]+$', args.seq):    # selecting the right function for RNA
+    percentage_a, percentage_u, percentage_c, percentage_g = calculate_nucleotide_percentage_RNA(args.seq)
+    print(f"Percentage of A:", percentage_a)
+    print(f"Percentage of U:", percentage_u)
+    print(f"Percentage of C:", percentage_c)
+    print(f"Percentage of G:",  percentage_g)
+else:
+    print("The sequence is wrong")
+
+
